46555787832ec52d672e2075c738430769b636d7
ndg1g24 <ndg1g24@soton.ac.uk>
2025-03-01 22:18:47 +0000

    ultrasonic

---
+++ ultrasonic.py
@@ -0,0 +1,7 @@
+from sbot import arduino
+
+def get_top_distance():
+    return arduino.measure_ultrasound_distance(2,3)
+
+def get_bottom_distance():
+    return arduino.measure_ultrasound_distance(8,9)
