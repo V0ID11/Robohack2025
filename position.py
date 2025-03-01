c7e288c3e33f5ac17d75f2e8ebba285a175c92e5
Sam Lockie <93869362+Slockie29@users.noreply.github.com>
2025-03-01 22:25:19 +0000

    Changed Position file

--- position.py
+++ position.py
@@ -1,2 +1,31 @@
-def get_location():
-    return 0
+from sbot import vision
+from sbot import marker as m
+
+def get_markers():
+    WallMarkers, BoxMarkers = [],[]
+    markers = vision.detect_markers()
+    for marker in markers:
+        if marker.id<=27:
+            WallMarkers.append(marker)
+        else:
+            BoxMarkers.append(marker)
+    return WallMarkers,BoxMarkers
+
+def find_closest(markers: list[m.Marker]):
+    closest_marker = markers[0]
+    for i in range(1,len(markers)-1):
+        if markers[i].position.distance < closest_marker.position.distance:
+            closest_marker = markers[i]
+    return closest_marker
+
+def direction_to_marker(marker:m.Marker):
+    angle = marker.position.horizontal_angle
+    if angle > 0:
+        return "r", angle
+    elif angle < 0:
+        return "l", abs(angle)
+    else:
+        return "s", 0
+
+        
+     
