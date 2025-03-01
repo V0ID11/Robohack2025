de9e469539967a1b08b8e2ac48660b2220f1d8d3
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-03-01 20:42:03 +0000

    added optional time

--- move.py
+++ move.py
@@ -1,14 +1,19 @@
 from sbot import *
+import time as t
 
-
-def move(direction: str, speed=0.25) -> None:
+def move(direction: str, speed=0.25,time=-1) -> None:
     directionAsInt = 1
     if direction.lower() == "f":
         directionAsInt = -1
 
     motors.set_power(0,speed*directionAsInt)
     motors.set_power(1,speed*directionAsInt)
+    if time > -1:
+        t.sleep(time)
+        stop()
+
 
 def stop() -> None:
     motors.set_power(0,0)
     motors.set_power(1,0)
+
