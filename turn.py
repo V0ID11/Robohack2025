925f3a85729b5fd93021a8902258a14d45e96e24
ndg1g24 <ndg1g24@soton.ac.uk>
2025-03-02 00:40:38 +0000

    Fixed turn on sam branch

--- turn.py
+++ turn.py
@@ -3,14 +3,18 @@ import time as t
 
 def turn(direction: str,angle=-1):
     directionAsInt = 1
-    if direction.lower() == "r":
+    if direction.lower() == "c":
         directionAsInt = -1
-    motors.set_power(0,0.5*directionAsInt)
-    motors.set_power(1,-0.5*directionAsInt)
     if angle > -1:
-        t.sleep(angle/360 * 1.5)
+        totaltime = angle/360 * 3.4
+        motors.set_power(0,0.25*directionAsInt)
+        motors.set_power(1,-0.25*directionAsInt)
+        t.sleep(totaltime)
         stop()
+    else:
+        motors.set_power(0,0.25*directionAsInt)
+        motors.set_power(1,-0.25*directionAsInt)
 
 def stop() -> None:
-    motors.set_power(0,0)
-    motors.set_power(1,0)
+    motors.set_power(0,BRAKE)
+    motors.set_power(1,BRAKE)
