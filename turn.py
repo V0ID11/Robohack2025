8352c1d002674863391bd3d550c2bead27e4b193
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-03-02 00:50:54 +0000

    turn better

--- turn.py
+++ turn.py
@@ -6,7 +6,7 @@ def turn(direction: str,angle=-1):
     if direction.lower() == "c":
         directionAsInt = -1
     if angle > -1:
-        totaltime = angle/360 * 3.4
+        totaltime = angle/360 * 3.41
         motors.set_power(0,0.25*directionAsInt)
         motors.set_power(1,-0.25*directionAsInt)
         t.sleep(totaltime)
