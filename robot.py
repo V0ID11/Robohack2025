91462a79cc79beb6929bb195bbcaca323aafd47a
Sam Lockie <93869362+Slockie29@users.noreply.github.com>
2025-03-02 01:28:39 +0000

    Angle Testing

--- robot.py
+++ robot.py
@@ -3,6 +3,8 @@ import time as zac
 import turn as t
 import position as p
 
-m.move(direction="f", time = 5, distance = 500)
+# m.move(direction="f", time = 5, distance = 500)
 
-
+marker = p.find_marker_of_id(106)
+angle = p.direction_to_marker()
+print("ANGLE: " + str(angle))
